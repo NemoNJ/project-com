46c6c4de3276d82e62c735b76c0fb175cb8ce429
MikePzaza <152591540+MikePzaza@users.noreply.github.com>
2024-02-17 17:32:05 +0700

    Update char.cpp

--- project_battlefield-03/char.cpp
+++ project_battlefield-03/char.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main(){
 
-
+//solder
 printf("     ------\n");
 printf("    /      '\n");
 printf("   /--------|      /|\n");
@@ -20,7 +20,6 @@ printf("   |==|      |\n");
 printf("   '=====[]=='\n");
 printf("   |         |\n");
 
-
 printf("     ------\n");
 printf("    /      '\n");
 printf("   /--------|\n");
@@ -34,6 +33,93 @@ printf("   |==|      |\n");
 printf("   |==|      |\n");
 printf("   '=====[]=='\n");
 printf("   |         |\n");
+
+printf("             ------\n");
+printf("      #     / -  - |  \n");
+printf("      #     | o  o | \n");
+printf("      #     |  -   |\n");
+printf("      #     '-----'\n");
+printf("      []----        |\n");
+printf("      []------      |\n");
+printf("      #     |       |\n");
+printf("            |       |\n");
+printf("            |       |\n");
+printf("            --------\n");
+printf("            |       |\n");
+
+printf("             /------\n");
+printf("           ---------|\n");
+printf("            | =  = |\n");
+printf("            |  -   |\n");
+printf("   '====     '-----'\n");
+printf("      '[]----       |\n");
+printf("        ------      |\n");
+printf("            |       |\n");
+printf("            |       |\n");
+printf("            |       |\n");
+printf("            --------\n");
+printf("            |       |\n");
+
+printf("               /------\n");
+printf("             ---------|\n");
+printf("              |[]-[] |\n");   
+printf("              |  -   |\n");
+printf(" -'=======q    '-----'\n");
+printf("   ||   '[]==---      |\n");
+printf("    --    ------      |\n");
+printf("              |       |\n");
+printf("              |       |\n");
+printf("              |       |\n");
+printf("              --------\n");
+printf("              |       |\n");
+
+
+//Pistol
+printf("          _______________==\n");
+printf("         q|[[[[ [__]   ___|\n");
+printf("         /_____________/_ |\n");
+printf("        |    [|_]'\n");
+printf("        | ## |\n");
+printf("        | ## |\n");
+printf("        |_##_|\n");
+printf("\n");
+
+//Rifile
+printf("                    __:\n");
+printf("          /**********|======.._____    ==\n");
+printf(" --------/  '/-----/ |=[][] !!-----|___/|\n");
+printf("|        __  |_]--|--|=======-----------|\n");
+printf("|      _/  |_|     |  |\n");
+printf("|_____/    ==       |__|\n");
+printf("\n");
+
+//Armor1
+printf("    ___     ___\n");
+printf("    [ ]_____[ ]]\n");
+printf("   |    VEST   ||\n");
+printf("   |           ||\n");
+printf("   |___ ___ ___||\n");
+printf("   |[__][__][__]|\n");
+printf("   |   |   |   ||\n");
+printf("   |___|___|___|/\n");
+
+
+//graned
+
+printf("  _______________\n");
+printf(" /______________/|\n");
+printf(" |      _      | |\n");
+printf(" |   __| |__   | |\n");
+printf(" |  |__   __|  | |\n");
+printf(" |     |_|     | |\n");
+printf(" |  first aid  | |\n");
+printf(" |_____________|/\n");
+printf(" \n");
+printf(" \n");
+
+
+printf(" \n");
+
 
 
 }
