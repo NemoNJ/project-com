6fedc1f573c045f5c3c7b63dc739566dee5d056c
nulys nulys <nulys@gmasdsdil.com>
2024-02-17 16:46:02 +0700

    3card

---
+++ 3Card.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << " ______________" << endl;
+    cout << "|              |" << endl;
+    cout << "|      __      |" << endl;
+    cout << "|   __|  |__   |" << endl;
+    cout << "|  |__    __|  |" << endl;
+    cout << "|     |__|     |" << endl;
+    cout << "|              |" << endl;
+    cout << "|______________|" << endl;
+    cout << " ______________" << endl;
+    cout << "|              |" << endl;
+    cout << "|      /\\      |" << endl;
+    cout << "|     |  |     |" << endl;
+    cout << "|   __|__|__   |" << endl;
+    cout << "|  |__|__|__|  |" << endl;
+    cout << "|     |__|     |" << endl;
+    cout << "|______________|" << endl;
+    cout << " ______________" << endl;
+    cout << "|  __________  |" << endl;
+    cout << "| |          | |" << endl;
+    cout << "| |  __||__  | |" << endl;
+    cout << "| |    ||    | |" << endl;
+    cout << "|  \\        /  |" << endl;
+    cout << "|   \\______/   |" << endl;
+    cout << "|______________|" << endl;
+}
