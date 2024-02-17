f880ce4b578a63d767b1bc9f3f6f8740f8d21a78
MikePzaza_ <pzaza49243002@gmail.com_>
2024-02-17 15:58:03 +0700

    upchar

--- project_battlefield-03/char.cpp
+++ project_battlefield-03/char.cpp
@@ -1,1 +1,39 @@
+#include<iostream>
+//#include"color.h" 
+#include <stdio.h>
+using namespace std;
 
+int main(){
+
+
+printf("     ------\n");
+printf("    /      '\n");
+printf("   /--------|      /|\n");
+printf("   |/   I I |      || \n");
+printf("   ||    -  /      || \n");
+printf("     '------'      +++\n");
+printf("    /  ------------[] \n");
+printf("   /  |      |----/ \n");
+printf("   |  |      |\n");
+printf("   |==|      |\n");
+printf("   |==|      |\n");
+printf("   '=====[]=='\n");
+printf("   |         |\n");
+
+
+printf("     ------\n");
+printf("    /      '\n");
+printf("   /--------|\n");
+printf("   |/   I I |\n");
+printf("   ||    -  /  \n");
+printf("     '------'       ===='\n");
+printf("    /  ------------[]' \n");
+printf("   /  |      |----/\n");
+printf("   |  |      |\n");
+printf("   |==|      |\n");
+printf("   |==|      |\n");
+printf("   '=====[]=='\n");
+printf("   |         |\n");
+
+
+}
