76a26a3ec542ee51b221ed63c792f8bd87bde09a
NemoNJ <152589734+NemoNJ@users.noreply.github.com>
2024-02-18 11:33:11 +0700

    Update main.cpp

--- project_battlefield-03/main.cpp
+++ project_battlefield-03/main.cpp
@@ -1,5 +1,4 @@
 #include "game03.h"
-#include "color.h"
 #include<iostream>
 #include<string>
 #include<ctime>
@@ -13,27 +12,21 @@
 #include <fstream>
 using namespace std;
 void condition(){
-    textcolor(RED,RED);
+
      printf("Press button A to ATTACK\n");
-     resetcolor();
-    textcolor(GREEN,GREEN);
+ 
      printf("Press button Q to USE MEDKIT\n");
-     resetcolor();
-      textcolor(YELLOW ,YELLOW);
+
      printf("Press button G to USE GRENADE\n");
-     resetcolor();
-     textcolor( BLUE,BLUE);
+
      printf("Press button S to SAVE GAME\n");
-     resetcolor();
-     textcolor(VIOLET,VIOLET );
+
      printf("Press button L to BLOAD GAME\n");
-     resetcolor();
-     textcolor(WHITE,WHITE);
+
      printf("Press button E to EXIT GAME\n");
-     resetcolor();
+
 }
 void terminal(){
-    textcolor(DARKRED,DARKRED);
     printf( "---------------------------------------------------------------------------------------------------\n");
     printf("|                                                                                                   |\n");
     printf("|                                                                                                   |\n");
@@ -47,7 +40,6 @@ void terminal(){
     printf("|                                                                                                   |\n");
     printf("|                                                                                                   |\n");
     printf( "---------------------------------------------------------------------------------------------------");
-    resetcolor();
 }
 string chooose_mon(int count){ //เลือก mons ที่จะบวก
     string boss;
